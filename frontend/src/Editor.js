1e4d7792ecc24d20f9beb608ca65969b5285be4f
Jakub Janarek <jjanarek@gmail.com>
2017-10-14 20:27:41 +0200

    stuff sdsdrfbreb

--- frontend/src/Editor.js
+++ frontend/src/Editor.js
@@ -97,7 +97,7 @@ class Editor extends Component {
       .then((canvas) => {
         const imgData = canvas.toDataURL('image/png');
         const pdf = new jsPDF();
-        pdf.addImage(imgData, 'JPEG', 2, 0);
+        pdf.addImage(imgData, 'JPEG', 0, 0);
         pdf.output('dataurlnewwindow');
       })
       .catch((error) => {
@@ -148,34 +148,3 @@ class Editor extends Component {
 }
 
 export default Editor;
-
-/*
-const app = new Application();
-// The application will create a canvas element for you that you
-// can then insert into the DOM.
-document.body.appendChild(app.view);
-document.getElementsByClassName
-// load the texture we need
-loader.add('bunny', 'bunny.png').load(function(loader, resources) {
-  // This creates a texture from a 'bunny.png' image.
-  var bunny = new Sprite(resources.bunny.texture);
-
-  // Setup the position of the bunny
-  bunny.x = app.renderer.width / 2;
-  bunny.y = app.renderer.height / 2;
-
-  // Rotate around the center
-  bunny.anchor.x = 0.5;
-  bunny.anchor.y = 0.5;
-
-  // Add the bunny to the scene we are building.
-  app.stage.addChild(bunny);
-
-  // Listen for frame updates
-  app.ticker.add(function() {
-        // each frame we spin the bunny around a bit
-      bunny.rotation += 0.01;
-  });
-});
-*/
-
